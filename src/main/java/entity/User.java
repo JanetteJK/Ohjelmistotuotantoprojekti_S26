207a47cedaa5f40d26746654f570f6033551f3a2
Fliits <aatuska576@gmail.com>
2026-09-10 12:26:10 +0300

    Add DAO and database connection classes for card management

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,24 @@
+package entity;
+
+public class User {
+    private String userName;
+
+    private String password;
+
+    private enum type{
+        student,
+        teacher
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
