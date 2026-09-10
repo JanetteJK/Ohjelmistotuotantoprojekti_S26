1932109a4967967b3195c2cc723e9ba55fe1ce24
Fliits <aatuska576@gmail.com>
2026-09-10 12:26:10 +0300

    Add DAO and database connection classes for card management

---
+++ src/main/java/entity/Card.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Card {
+    private String answer;
+    private String question;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
