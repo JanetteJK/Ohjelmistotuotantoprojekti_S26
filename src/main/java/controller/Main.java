b23d78753783cefd208563b2b9e425ddd749cbf5
janetjk <janetjk@metropolia.fi>
2026-09-20 14:16:16 +0300

    kokeilkaa

--- src/main/java/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import view.SignIn;
-
-public class Main {
-    static void main(String[] args) {
-        SignIn.main(args);
-    }
-}
--- src/main/java/controller/signInController.java
+++ src/main/java/controller/signInController.java
@@ -53,7 +53,6 @@ public class signInController {
         if(actionEvent.getSource()==tOk) {
             String un = gettUsername();
             PasswordField ps = gettPassw();
-            User teacher = new User();
             // todo: here the thing that I talked about w sonja
 
 
