c42bf56418054f168a815f948654500780792e80
sonjameskus <sonja.meskus@metropolia.fi>
2026-09-23 13:15:24 +0300

    lisätty juttuja

--- src/main/java/dao/CardDao.java
+++ src/main/java/dao/CardDao.java
@@ -26,6 +26,67 @@ public class CardDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public static void modifyCard(Card c) {
+        if (conn == null) {
+            System.out.println("Connection is null!");
+            return;
+        }
+
+        String sql = "UPDATE flashcards SET question = ?, answer = ?, category = ? WHERE fc_id = ?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, c.getQuestion());
+            stmt.setString(2, c.getAnswer());
+            stmt.setString(3, c.getCategory());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showAllCards(Card c) {
+        if (conn == null) {
+            System.out.println("Connection is null!");
+            return;
+        }
+
+        String sql = "SELECT (question, answer, category) FROM flashcards WHERE user_id = ?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, c.getUserId());
+
+            stmt.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showCardsBasedOnCategory(Card c) {
+        if (conn == null) {
+            System.out.println("Connection is null!");
+            return;
+        }
+
+        String sql = "SELECT (question, answer, category) FROM flashcards WHERE user_id = ? AND category = ?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, c.getUserId());
+            stmt.setString(2, c.getCategory());
+
+            stmt.executeQuery();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
