bf4b87e75587dc2167771db84beb28b24513f695
sonjameskus <sonja.meskus@metropolia.fi>
2026-09-24 11:20:11 +0300

    testi

--- src/test/java/entity/UserTest.java
+++ src/test/java/entity/UserTest.java
@@ -7,13 +7,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserTest {
 
     @Test
-    void testUserGetters() {
+    void testUserGettersAndSetters() {
         User u = new User("testiukko", "testiukko@email.fi", "testiukonsalasana", student);
 
     assertEquals("testiukko", u.getUserName());
     assertEquals("testiukko@email.fi", u.getEmail());
     assertEquals("testiukonsalasana", u.getPassword());
     assertEquals(student, u.getRole());
+
+    u.setUserName("testiakka");
+    u.setEmail("testiakka@email.fi");
+    u.setPassword("testiakansalasana");
+    u.setRole(teacher);
+    assertEquals("testiakka", u.getUserName());
+    assertEquals("testiakka@email.fi", u.getEmail());
+    assertEquals("testiakansalasana", u.getPassword());
+    assertEquals(teacher, u.getRole());
     }
 
 }
